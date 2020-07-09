7a575e83dfbfeee98c3d9f8230576757bd62ece5
Rydel-16 <68080423+Rydel-16@users.noreply.github.com>
2020-07-09 18:55:23 -0500

    Pregunta01 - Listo

--- Preg01.py
+++ Preg01.py
@@ -1,8 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul  9 18:50:48 2020
+Created on Thu Jul  9 17:26:56 2020
 
-@author: USUARIO
+@author: Rhisto Zea
 """
 
+def primos(numero):
+    c=0
+    if int(numero) < 1:
+        validar = "Numero no valido."
+        return validar
+    elif numero == 2:
+        validar = "No es primo."
+        return validar
+    else:
+        for i in range(2, int(numero)):
+            if int(numero)%i == 0:
+                c=+1
 
+    if c>=1:
+        validar = "No es primo."
+        return validar
+    else:
+        validar = "Es primo"
+        return validar
+
+numero = input("Ingrese un numero: ")
+print(primos(numero))
