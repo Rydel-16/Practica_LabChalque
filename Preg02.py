3d3f465837853ad5a153e8132cacc614866f046e
Rydel-16 <68080423+Rydel-16@users.noreply.github.com>
2020-07-09 18:56:39 -0500

    Pregunta02 - Listo

--- Preg02.py
+++ Preg02.py
@@ -1,8 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul  9 18:50:59 2020
+#Autor: Vilca 
 
-@author: USUARIO
-"""
+def comprobar():
+    correo=input(f"Ingrese correo electronico: ")
 
+    tamaño=len(correo)
+    tamaño1=len(correo[0:(len(correo)-13)])
+    direccion=(correo[(len(correo)-14):(len(correo))])
+
+    if tamaño<14:
+        print("No es correo de la untels")
+    elif direccion=="@untels.edu.pe":
+       print("Sí es correo de la untels") 
+    else:
+       print("No es correo de la untels") 
+        
+comprobar()
 
