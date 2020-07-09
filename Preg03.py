87c4779d9b8af54a3c7874d26ba18d73538c9e2d
Rydel-16 <68080423+Rydel-16@users.noreply.github.com>
2020-07-09 18:58:21 -0500

    Pregunta03 - Listo

--- Preg03.py
+++ Preg03.py
@@ -1,8 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul  9 18:51:44 2020
+#  Diseñar un programa que permita mostrar la cantidad de letras que tiene una cadena
+# Autor: rose
 
-@author: USUARIO
-"""
+def  contar_digitos(cadena):
+    digitos = 0
+    letras = 0
+    
+    for c in cadena:
+        if c.isdigit():
+            digitos += 1
+        elif c.isalpha():
+            letras += 1
+        else:
+            pass
+        
+    return digitos, letras
 
+texto = input('digite un texto: ')
 
+resultado = contar_digitos(texto)
+print("cantidad de digitos: %i" % resultado[0])
+print("cantidad de letras: %i" % resultado[1])
